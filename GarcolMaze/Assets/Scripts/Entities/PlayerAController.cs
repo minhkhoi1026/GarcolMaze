2004378f5aaa624ae0caae934c1a66352e5fb806
Thành Nguyễn Đăng Tiến <tienthanhcute214@gmail.com>
2022-10-19 10:15:10 +0700

    feat: character movement

--- GarcolMaze/Assets/Scripts/Entities/PlayerAController.cs
+++ GarcolMaze/Assets/Scripts/Entities/PlayerAController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerAController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    Rigidbody2D rigidbody;
+    Animator animator;
+
+    Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+        if (movement.sqrMagnitude > 0)
+		{
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetBool("IsMoving", true);
+		} else
+		{
+            animator.SetBool("IsMoving", false);
+		}
+
+        // update animation
     }
+
+	private void FixedUpdate()
+	{
+		if (movement != null)
+		{
+            if (movement.SqrMagnitude() > 0)
+			{
+                Debug.Log(movement);
+
+            }
+            rigidbody.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+		}
+	}
 }
