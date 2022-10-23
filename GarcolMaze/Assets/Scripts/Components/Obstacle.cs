05b18e0e448b1aea1096f5036cf0f9d391e9b284
binhlecong <congbinh21012001@gmail.com>
2022-10-23 17:27:42 +0700

    add sand prefabs, change floor sprite

---
+++ GarcolMaze/Assets/Scripts/Components/Obstacle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Obstacle : MonoBehaviour
+{
+    Collider2D _collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        _collider.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        applyEffect(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        revertEffect(other);
+    }
+
+    abstract public void applyEffect(Collider2D other);
+    abstract public void revertEffect(Collider2D other);
+}
