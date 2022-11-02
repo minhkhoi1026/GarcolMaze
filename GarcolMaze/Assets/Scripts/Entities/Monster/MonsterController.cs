aa3fc671c3d94420d986aa817399a895f99a6fc5
Thành Nguyễn Đăng Tiến <tienthanhcute214@gmail.com>
2022-11-02 19:38:48 +0700

    feat: public damage point for monster

--- GarcolMaze/Assets/Scripts/Entities/Monster/MonsterController.cs
+++ GarcolMaze/Assets/Scripts/Entities/Monster/MonsterController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 
 public abstract class MonsterController : MonoBehaviour {
+	public int damagePoint = 5;
 	void Start() {
 	
 	}
@@ -15,4 +16,14 @@ public abstract class MonsterController : MonoBehaviour {
 	}
 
 	public abstract void Freeze(float freezeTime);
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag.StartsWith("Player"))
+		{
+			PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+			if (player != null)
+				player.Damage(damagePoint);
+		}
+	}
 }
